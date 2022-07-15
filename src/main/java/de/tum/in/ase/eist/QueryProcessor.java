949e0a03e2e4c3a8424e09edc3b8dae02bf5585d
oDuerer <oliver.duerer@tum.de>
2022-07-15 11:38:49 +0200

    new plus

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -6,28 +6,27 @@ import org.springframework.stereotype.Service;
 public class QueryProcessor {
 
     public String process(String query) {
-		query = query.toLowerCase();
+        query = query.toLowerCase();
         if (query.contains("shakespeare")) {
             return "William Shakespeare (26 April 1564 - 23 April 1616) was an " +
                     "English poet, playwright, and actor, widely regarded as the greatest " +
                     "writer in the English language and the world's pre-eminent dramatist.";
         } else if (query.contains("name")) {
             return "Oliver";
-        }
-        else if (query.contains("plus")) {
-            String[] nums = query.split("plus");
+        } else if (query.contains("plus")) {
+            String[] nums = query.substring(8).split("plus");
             try {
                 int num = Integer.parseInt(nums[1]) + Integer.parseInt(nums[0]);
                 return Integer.toString(num);
             } catch (NumberFormatException e) {
-                e.printStackTrace();
+
             }
             return "";
         } else if (query.contains("largest")) {
             String subString = query.split(":")[1];
             String[] nums = subString.split(",");
             int max = Integer.parseInt(nums[0]);
-            try{
+            try {
                 for (String num :
                         nums) {
                     if (Integer.parseInt(num) > max) {
@@ -36,7 +35,6 @@ public class QueryProcessor {
                 }
                 return Integer.toString(max);
             } catch (NumberFormatException e) {
-                e.printStackTrace();
                 return "";
             }
 
